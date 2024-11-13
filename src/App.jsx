33eb7db681c14791e7eac78c6071f01c83a04f8a
unknown <artemaskel@gmail.com>
2024-11-13 21:47:02 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,10 @@ function App() {
   const [out, setOut] = useState("")
 
   tg.expand();
-  tg.MainButton.show()
 
   tg.MainButton.text = "Отправить"; //изменяем текст кнопки 
-  tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-  tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-  tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+  tg.MainButton.textColor = "#FFF"; //изменяем цвет текста кнопки
+  tg.MainButton.color = "#16295D"; //изменяем цвет бэкграунда кнопки
 
   function send(){
     const name = document.querySelector('.name').value
@@ -34,10 +32,10 @@ function App() {
     }
 
 
-    tg.sendData({
-      // chat_id: '1768792009',
-      text: `Имя: ${name}\nКонтакт: ${contact}\nВопрос: ${question}`
-    })
+    // tg.sendData({
+    //   // chat_id: '1768792009',
+    //   text: `Имя: ${name}\nКонтакт: ${contact}\nВопрос: ${question}`
+    // })
 
 
     if(done == 3){
