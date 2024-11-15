03505885583e11928ed629d8b44ebaedc2e13b7e
unknown <artemaskel@gmail.com>
2024-11-15 18:56:47 +0300

    aus

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,22 @@ function App() {
 
   function send(){
     const name = document.querySelector('.name').value
-    const contact = document.querySelector('.contact').value
+    // const contact = document.querySelector('.contact').value
     const question = document.querySelector('.question').value
 
     let done = 0
     if(name != ""){
       done++
     }
-    if(contact != ""){
-      done++
-    }
+    // if(contact != ""){
+    //   done++
+    // }
     if(question != ""){
       done++
     }
 
-    if(done == 3){
-      // tg.sendData(JSON.stringify({
-      //   // chat_id: '1768792009',
-      //   text: `Имя: ${name}\nКонтакт: ${contact}\nВопрос: ${question}`
-      // }))
-      tg.sendData(JSON.stringify({
-        name: name,
-        contact: contact,
-        question: question,
-      }))
+    if(done == 2){
+      tg.sendData(`${name} спрашивает: ${question}`)
     } else {
       setOut("Все поля должны быть заполнены!")
     }
@@ -50,8 +42,8 @@ function App() {
         <div className="fields">
         <p className='out'>{out}</p>
           <input type="text" placeholder='Имя' className='name'/>
-          <input type="text" placeholder='Телефон/e-mail' className='contact'/>
-          <textarea name="question" id="questionDesc" placeholder='Ваш вопрос' className='question' rows={4}></textarea>
+          {/* <input type="text" placeholder='Телефон/e-mail' className='contact'/> */}
+          <textarea name="question" id="questionDesc" placeholder='Ваш вопрос' className='question' rows={6}></textarea>
         <button type="submit" className='send' onClick={send}>Отправить</button>
         </div>
     </div>
